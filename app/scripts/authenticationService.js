2b3a5abb47403da391ac49001deba081d5cca2f2
David Spies <david.m.spies@gmail.com>
2014-03-26 08:22:42 -0700

    Added authentication service and tests

---
+++ app/scripts/authenticationService.js
@@ -0,0 +1,54 @@
+  angular.module('ua.security')
+    .provider('authenticationService', [function(){
+
+      var authenticationUrl = '/login';
+      this.setAuthenticationUrl = function(value){
+        authenticationUrl = value;
+      };
+
+      var logoutUrl = '/logout';
+      this.setLogoutUrl = function(value){
+        logoutUrl = value;
+      };
+
+      function AuthenticationService($http, $q){
+
+        this.authenticate = function(username, password){
+
+          var deferred = $q.defer();
+
+          var user = {
+            username: username,
+            password: password
+          };
+
+          $http.post(authenticationUrl, user)
+              .success(function(data){
+                return deferred.resolve(data);
+              })
+              .error(function(data, status){
+                return deferred.reject(status);
+              });
+
+          return deferred.promise;
+        };
+
+        this.logout = function(){
+          var deferred = $q.defer();
+
+          $http.post(logoutUrl)
+              .success(function(data){
+                deferred.resolve(data);
+              })
+              .error(function(data, status){
+                deferred.reject(status);
+              });
+
+          return deferred.promise;
+        };
+      }
+
+      this.$get = ['$http', '$q', function AuthenticationServiceFactory($http, $q){
+        return new AuthenticationService($http, $q);
+      }];
+    }]);
---
+++ test/spec/authenticationService.spec.js
@@ -0,0 +1,291 @@
+(function(){
+
+  'use strict';
+
+  //Test the authentication service without supplying configuration
+  // variables.  The authentication service will use its defaults
+  describe('authenticationService without configuration', function(){
+
+    var authenticationService;
+    var $httpBackend;
+
+    //load the ua.security module
+    beforeEach(module('ua.security'));
+
+    beforeEach(function(){
+      //inject your service and mocks for testing
+      inject(function(_authenticationService_, _$httpBackend_){
+        authenticationService = _authenticationService_;
+
+        $httpBackend = _$httpBackend_;
+      });
+    });
+
+    //define authentication url in configuration
+    describe('authenticate', function(){
+
+      var username = 'user';
+      var password = 'password';
+      var userCredentials = {
+        username: username,
+        password: password
+      };
+      var successfulUserReturned = {
+        username: username,
+        token: 'IAmAHappyToken',
+        roles: ['ROLE_USER', 'ROLE_ADMIN']
+      };
+
+      it('function exists', function(){
+        console.info('Checking whether the function exists');
+        expect(angular.isFunction(authenticationService.authenticate)).toBe(true);
+      });
+
+      it('is successful with good credentials and returns a promise from authentication endpoint', function(){
+
+        $httpBackend.expectPOST('/login', userCredentials)
+            .respond(200, successfulUserReturned);
+
+        var returnedPromise = authenticationService.authenticate(username, password);
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(function(data){
+          expect(data).not.toBeNull();
+        });
+
+        $httpBackend.flush();
+      });
+
+      it('fails to authenticate with bad credentials and returns 403 error from authentication endpoint', function(){
+
+        $httpBackend.expectPOST('/login', userCredentials)
+            .respond(403, '');
+
+        var returnedPromise = authenticationService.authenticate(username, password);
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(
+            function(){},
+            function(reason){
+              expect(reason).toBe(403);
+            });
+
+        $httpBackend.flush();
+
+      });
+
+    });
+
+    describe('logout', function(){
+      it('function exists', function(){
+        expect(angular.isFunction(authenticationService.logout)).toBe(true);
+      });
+
+      it('is successful with a header auth token and returns a promise from the logout endpoint', function(){
+
+        var $http;
+        var authTokenHeaders = {'x-auth-token': 'IAmYourToken'};
+
+        inject(function(_$http_){
+          $http = _$http_;
+          $http.defaults.headers.common = authTokenHeaders;
+        });
+
+        $httpBackend.expectPOST('/logout',
+            undefined,
+            authTokenHeaders
+        ).respond(200, '');
+
+        var returnedPromise = authenticationService.logout();
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(function(data){
+          expect(data).toBe('');
+        });
+
+        $httpBackend.flush();
+
+      });
+
+      it('fails without a header auth token and returns 404 error from logout endpoint', function(){
+
+        var $http;
+        var authTokenHeaders = {'x-auth-token': 'IAmYourToken'};
+
+        inject(function(_$http_){
+          $http = _$http_;
+          $http.defaults.headers.common = authTokenHeaders;
+        });
+
+        $httpBackend.expectPOST('/logout',
+            undefined,
+            authTokenHeaders
+        ).respond(404, '');
+
+        var returnedPromise = authenticationService.logout();
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(
+            function(){},
+            function(message){
+              expect(message).toBe(404);
+            });
+
+        $httpBackend.flush();
+
+      });
+
+    });
+
+    afterEach(function(){
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+  });
+
+  //Test the authentication service with the authenticationUrl and
+  // logoutUrl provided to the service via configuration variables
+  describe('authenticationService with configuration', function(){
+
+    var authenticationService;
+    var $httpBackend;
+
+    //load the ua.security module
+    beforeEach(module('ua.security', function(authenticationServiceProvider){
+      authenticationServiceProvider.setAuthenticationUrl('/myLogin');
+      authenticationServiceProvider.setLogoutUrl('/myLogout');
+    }));
+
+    beforeEach(function(){
+      //inject your service and mocks for testing
+      inject(function(_authenticationService_, _$httpBackend_){
+        authenticationService = _authenticationService_;
+
+        $httpBackend = _$httpBackend_;
+      });
+    });
+
+    //define authentication url in configuration
+    describe('authenticate', function(){
+
+      var username = 'user';
+      var password = 'password';
+      var userCredentials = {
+        username: username,
+        password: password
+      };
+      var successfulUserReturned = {
+        username: username,
+        token: 'IAmAHappyToken',
+        roles: ['ROLE_USER', 'ROLE_ADMIN']
+      };
+
+      it('function exists', function(){
+        expect(angular.isFunction(authenticationService.authenticate)).toBe(true);
+      });
+
+      it('is successful with good credentials and returns a promise from authentication endpoint', function(){
+
+        $httpBackend.expectPOST('/myLogin', userCredentials)
+            .respond(200, successfulUserReturned);
+
+        var returnedPromise = authenticationService.authenticate(username, password);
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(function(data){
+          expect(data).not.toBeNull();
+        });
+
+        $httpBackend.flush();
+      });
+
+      it('fails to authenticate with bad credentials and returns 403 error from authentication endpoint', function(){
+
+        $httpBackend.expectPOST('/myLogin', userCredentials)
+            .respond(403, '');
+
+        var returnedPromise = authenticationService.authenticate(username, password);
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(
+            function(){},
+            function(reason){
+              expect(reason).toBe(403);
+            });
+
+        $httpBackend.flush();
+
+      });
+
+    });
+
+    describe('logout', function(){
+      it('function exists', function(){
+        expect(angular.isFunction(authenticationService.logout)).toBe(true);
+      });
+
+      it('is successful with a header auth token and returns a promise from the logout endpoint', function(){
+
+        var $http;
+        var authTokenHeaders = {'x-auth-token': 'IAmYourToken'};
+
+        inject(function(_$http_){
+          $http = _$http_;
+          $http.defaults.headers.common = authTokenHeaders;
+        });
+
+        $httpBackend.expectPOST('/myLogout',
+            undefined,
+            authTokenHeaders
+        ).respond(200, '');
+
+        var returnedPromise = authenticationService.logout();
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(function(data){
+          expect(data).toBe('');
+        });
+
+        $httpBackend.flush();
+
+      });
+
+      it('fails without a header auth token and returns 404 error from logout endpoint', function(){
+
+        var $http;
+        var authTokenHeaders = {'x-auth-token': 'IAmYourToken'};
+
+        inject(function(_$http_){
+          $http = _$http_;
+          $http.defaults.headers.common = authTokenHeaders;
+        });
+
+        $httpBackend.expectPOST('/myLogout',
+            undefined,
+            authTokenHeaders
+        ).respond(404, '');
+
+        var returnedPromise = authenticationService.logout();
+        expect(angular.isFunction(returnedPromise.then)).toBe(true);
+
+        returnedPromise.then(
+            function(){},
+            function(message){
+              expect(message).toBe(404);
+            });
+
+        $httpBackend.flush();
+
+      });
+
+    });
+
+    afterEach(function(){
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+  });
+
+}());
