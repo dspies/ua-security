343bc77fbeb093a393fad7cc6b7aa3f960a8a2b5
David Spies <david.m.spies@gmail.com>
2014-04-17 01:54:00 -0700

    Updated site
    Added Bower installation, logout configuration, and changed some language

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,37 @@ module.exports = function(grunt) {
             options: {
                 port: 35270
             },
-            web: {
-                files: ['/*.js', 'stylesheets/**', 'javascripts/**'],
-                tasks: ['default']
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: ['*.html', 'stylesheets/**', 'javascripts/**']
+            }
+        },
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true
+                }
             }
         }
     });
 
     // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', [
+        'connect:livereload',
+        'watch'
+    ]);
 
 };
