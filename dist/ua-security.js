e6ed94ba600a3ef6ceb44186eb074add4969f735
David Spies <david.m.spies@gmail.com>
2014-04-18 02:11:07 -0700

    Release v0.1.1

--- dist/ua-security.js
+++ dist/ua-security.js
@@ -1,6 +1,6 @@
 /**
  * Simple token-based security service for AngularJS apps
- * @version v0.1.0 - 2014-04-18
+ * @version v0.1.1 - 2014-04-18
  * @link https://github.com/dspies/ua-security
  * @author David Spies <david.m.spies@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
