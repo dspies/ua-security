9b1d7651cd9de0df97b63dbb164c7976037f80ff
David Spies <david.m.spies@gmail.com>
2014-10-31 12:15:50 -0700

    Fixed Bower package name

--- dist/angular-ua-security.js
+++ dist/angular-ua-security.js
@@ -1,6 +1,6 @@
 /**
  * Simple token-based security service for AngularJS apps
- * @version v0.1.6 - 2014-04-29
+ * @version v0.1.6 - 2014-10-31
  * @link https://github.com/dspies/ua-security
  * @author David Spies <david.m.spies@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
